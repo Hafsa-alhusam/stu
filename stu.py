41ca423797f83454efd4289aeec425f1613bf9ba
Hafsa Alhusam <hafsa.alhusam793@gmail.com>
2023-10-16 12:21:18 +0300

    class student

---
+++ stu.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, name, roll_number, age, grade):
+        self.name = name
+        self.roll_number = roll_number
+        self.age = age
+        self.grade = grade
