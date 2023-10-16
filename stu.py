eee3fd1fb7d3650cbbe4beeacb33d1cd1bafad8c
Hafsa Alhusam <hafsa.alhusam793@gmail.com>
2023-10-16 12:27:51 +0300

    def display_student

--- stu.py
+++ stu.py
@@ -19,3 +19,15 @@ class StudentManagementSystem:
         student = Student(name, roll_number, age, grade)
         self.students.append(student)
         print("Student added successfully!")
+
+    def display_students(self):
+        print("Student List:")
+        for student in self.students:
+            print(f"Name: {student.name}, Roll Number: {student.roll_number}, Age: {student.age}, Grade: {student.grade}")
+
+    def search_student(self, roll_number):
+        for student in self.students:
+            if student.roll_number == roll_number:
+                print(f"Student found - Name: {student.name}, Roll Number: {student.roll_number}, Age: {student.age}, Grade: {student.grade}")
+                return
+        print("Student not found!")
