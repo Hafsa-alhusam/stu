9744dac48040d9a28019ed233ddebb929c6a4d43
Hafsa Alhusam <hafsa.alhusam793@gmail.com>
2023-10-16 12:23:02 +0300

    class student mangement system

--- stu.py
+++ stu.py
@@ -4,3 +4,18 @@ class Student:
         self.roll_number = roll_number
         self.age = age
         self.grade = grade
+
+class StudentManagementSystem:
+    def __init__(self):
+        self.students = []
+
+    def accept_student(self):
+        print("Enter student details:")
+        name = input("Name: ")
+        roll_number = input("Roll Number: ")
+        age = input("Age: ")
+        grade = input("Grade: ")
+
+        student = Student(name, roll_number, age, grade)
+        self.students.append(student)
+        print("Student added successfully!")
